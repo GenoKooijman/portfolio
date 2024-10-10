1a04d6aa5d92d41433f3479f532d17ddc1d86334
GenoKooijman <genokooijman@gmail.com>
2024-10-10 16:03:44 +0200

    pushtogit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+ <header></header>
+ <div class="grid gap-8 grid-cols-1 lg:grid-cols-3 p-6"></div>
+  </div>
+</template>
