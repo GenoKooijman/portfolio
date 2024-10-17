f1923b315a738f18ca6c917e264fbf289c69fca5
GenoKooijman <genokooijman@gmail.com>
2024-10-17 14:56:28 +0200

    17-10-2024 end of day got rest api working

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, nextTick } from 'vue'
 import { useRuntimeConfig } from '#app'
 
-const posts = ref([])
+interface Post {
+  id: number;
+  title: {
+    rendered: string;
+  };
+  content: {
+    rendered: string;
+  };
+}
+
+const posts = ref<Post[]>([]) 
 
 onMounted(async () => {
   const config = useRuntimeConfig()
@@ -17,6 +27,15 @@ onMounted(async () => {
 
     const data = await response.json()
     posts.value = data
+
+ 
+    await nextTick()
+
+    
+    const namePhotoElement = document.getElementById('namePhoto')
+    if (namePhotoElement) {
+      namePhotoElement.classList.add('text-red-500', 'font-bold', 'text-xl') 
+    }
   } catch (error) {
     console.error('Error fetching data:', error)
   }
@@ -27,11 +46,11 @@ onMounted(async () => {
   <div>
     <h1>Pages</h1>
     <ul>
-      <li v-for="post in posts" :key="post.id">
-        <h2>{{ post.title.rendered }}</h2>
+      <li class="mb-8" v-for="post in posts" :key="post.id">
+        <h2 class="bg-black w-64 h-12 text-white pt-3">{{ post.title.rendered }}</h2>
         <div v-html="post.content.rendered"></div>
       </li>
     </ul>
-  </div>
+  </div>  
 </template>
 
--- src/composables/useHttp.ts
+++
@@ -1,7 +0,0 @@
-// src/composables/useHttp.ts
-import { useNuxtApp } from '#app'
-
-export function useHttp() {
-  const { $http } = useNuxtApp()
-  return $http
-}
