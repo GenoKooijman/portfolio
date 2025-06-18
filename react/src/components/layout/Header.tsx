e2f5708dbb66419b348e2bb72aa505130bf62b54
GenoKooijman <genokooijman@gmail.com>
2025-06-18 15:33:06 +0200

    Basic header added. Will change more in the future.

--- react/src/components/layout/Header.tsx
+++ react/src/components/layout/Header.tsx
@@ -1,17 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { navItems } from "@/data/navItems";
 
 export default function Header() {
+  const location = useLocation();
+
   return (
     <header>
       <nav>
-        <ul className="flex gap-2 font-bold">
-          {navItems.map((item) => (
-            <li key={item.to}>
-              <Link to={item.to}>{item.label}</Link>
-            </li>
-          ))}
-        </ul>
+        <div className="relative flex items-center w-full h-24">
+          <a href="/" className="absolute left-0 top-1/2 -translate-y-1/2">
+            <img src="img/logos/gkblacklogo.png" alt="logo" className="h-18" />
+          </a>
+          <ul className="flex justify-center gap-12 font-normal w-full">
+            {navItems.map((item) => {
+              const isActive = location.pathname === item.to;
+              return (
+                <li
+                  className={`text-black text-2xl font-montserrat ${
+                    isActive ? "border-b-2 border-black" : ""
+                  }`}
+                  key={item.to}
+                >
+                  <Link to={item.to}>{item.label}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </nav>
     </header>
   );
--- react/src/data/navItems.ts
+++ react/src/data/navItems.ts
@@ -3,4 +3,5 @@ import type { NavItem } from "@/types/types";
 export const navItems: NavItem[] = [
   { to: "/", label: "Home" },
   { to: "/about", label: "About" },
+  { to: "/projects", label: "Projects"},
 ];
