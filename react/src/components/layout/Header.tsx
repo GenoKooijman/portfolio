a527b76336093220a30814494303a74245dc7c09
GenoKooijman <genokooijman@gmail.com>
2025-06-19 11:51:37 +0200

    added gsap animation.

--- react/src/components/layout/Header.tsx
+++ react/src/components/layout/Header.tsx
@@ -1,11 +1,24 @@
 import { Link, useLocation } from "react-router-dom";
 import { navItems } from "@/data/navItems";
+import { gsap } from "gsap";
+import { useEffect, useRef} from "react";
 
 export default function Header() {
   const location = useLocation();
+  const headerRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+  if (headerRef.current) {
+    gsap.fromTo(
+      headerRef.current,
+      { y: -100, opacity: 0 },
+      { y: 0, opacity: 1, duration: 1, ease: "power2.out" }
+    );
+  }
+}, []);
 
   return (
-    <header>
+    <header ref={headerRef} id="main-header">
       <nav>
         <div className="relative flex items-center w-full h-24">
           <a href="/" className="absolute left-0 top-1/2 -translate-y-1/2">
@@ -16,7 +29,7 @@ export default function Header() {
               const isActive = location.pathname === item.to;
               return (
                 <li
-                  className={`text-black text-2xl font-montserrat ${
+                  className={`text-black text-3xl font-semibold font-montserrat ${
                     isActive ? "border-b-2 border-black" : ""
                   }`}
                   key={item.to}
