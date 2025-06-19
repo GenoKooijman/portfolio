d523f82b1987f20277d7b78390ee6262f9d3e00a
GenoKooijman <genokooijman@gmail.com>
2025-06-19 09:56:23 +0200

    added lenis smooth scroll + gsap.

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,10 +1,26 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import Home from "@/pages/Home";
 import About from "@/pages/About";
+import Lenis from "@studio-freight/lenis";
 
 export default function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+  
   return (
     <BrowserRouter>
       <Header />
