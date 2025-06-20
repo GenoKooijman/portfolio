85202fc32f705ab4f49d57cb9f272f5be2c64095
GenoKooijman <genokooijman@gmail.com>
2025-06-20 10:34:34 +0200

    added homeskills, made a section that shows my skills with animation.

---
+++ react/src/components/home/HomeSkills.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const ICON_IMAGES = [
+  "php.png",
+  "react.png",
+  "svelte.png",
+  "tailwind.png",
+  "vue.png",
+  "wordpress.png",
+  "git.png",
+  "gsap.png",
+  "mysql.png",
+  "npm.png",
+  "typescript.png",
+  "vite.png",
+  "wordpress.png",
+  "bun.png",
+  "phpmyadmin.png"
+];
+
+const GRID_IMAGES = Array.from({ length: 15 }, (_, i) => ICON_IMAGES[i % ICON_IMAGES.length]);
+
+export default function HomeSkills() {
+  const blocksRef = useRef<HTMLDivElement[]>([]);
+
+  useEffect(() => {
+    blocksRef.current.forEach((block, i) => {
+      if (!block) return;
+
+      gsap.set(block, { opacity: 0, y: 40 });
+
+      ScrollTrigger.create({
+        trigger: block,
+        start: "top 90%",
+        onEnter: () => {
+          gsap.to(block, {
+            opacity: 1,
+            y: 0,
+            delay: (i % 5) * 0.2,
+            duration: 0.6,
+            ease: "power3.out",
+          });
+        },
+        once: true, 
+      });
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, []);
+
+  return (
+    <section
+      id="skills-grid"
+      className="w-screen h-[120vh] grid grid-cols-5 grid-rows-3 gap-0"
+    >
+      {GRID_IMAGES.map((img, i) => (
+        <div
+          key={i}
+          ref={el => {
+            if (el) blocksRef.current[i] = el;
+          }}
+          className="flex items-center justify-center w-full h-full bg-white box-border hover:bg-gray-200 transition-colors shadow-lg"
+        >
+          <img
+            src={`/img/homeimg/${img}`}
+            alt={img.replace('.png', '')}
+            className="w-auto h-[50%] object-cover block"
+          />
+        </div>
+      ))}
+    </section>
+  );
+}
--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import HomeHero from "@/components/home/HomeHero";
 import HomeAbout from "@/components/home/HomeAbout";
+import HomeSkills from "@/components/home/HomeSkills";
 
 export default function Home() {
   return (
     <main>
       <HomeHero />
       <HomeAbout />
+      <HomeSkills />
     </main>
   );
 }
